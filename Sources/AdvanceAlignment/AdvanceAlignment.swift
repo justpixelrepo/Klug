326ad5076895e4af19afbcc5310ff4ffe9394d83
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-17 09:17:33 +0000

    added a FrameKey to track UUID and CGRect size

--- Sources/AdvanceAlignment/AdvanceAlignment.swift
+++ Sources/AdvanceAlignment/AdvanceAlignment.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Evans Domina Attafuah on 16/02/2022.
 //
@@ -11,7 +11,7 @@ struct Tree<A>: Identifiable {
     var value: A
     var children = [Tree<A>]()
     var id = UUID()
-    
+
     init(value: A, children: [Tree<A>] = []) {
         self.value = value
         self.children = children
@@ -26,7 +26,7 @@ let sample = Tree(value: "Root", children: [
 struct Line: Shape {
     var from: CGPoint
     var to: CGPoint
-    
+
     func path(in rect: CGRect) -> Path {
         .init { p in
             p.move(to: from)
@@ -35,19 +35,47 @@ struct Line: Shape {
     }
 }
 
+struct FrameKey: PreferenceKey {
+    static func reduce(value: inout [UUID: CGRect], nextValue: () -> [UUID: CGRect]) {
+        value.merge(nextValue(), uniquingKeysWith: { $1 })
+    }
+
+    static var defaultValue: [UUID: CGRect] { [:] }
+}
+
+extension View {
+    func measureFrame(in coordinateSpace: CoordinateSpace, id: UUID) -> some View {
+        background(
+            GeometryReader { proxy in
+                Color.clear.preference(key: FrameKey.self, value: [id: proxy.frame(in: coordinateSpace)])
+            }
+        )
+    }
+}
+
 struct Diagram<A, Node: View>: View {
     var tree: Tree<A>
     @ViewBuilder var node: (A) -> Node
     let coordinateSpace = "diagram"
-    
+
     var body: some View {
         VStack(spacing: 20) {
             node(tree.value)
+                .measureFrame(in: .named(coordinateSpace), id: tree.id)
             HStack(spacing: 20) {
                 ForEach(tree.children) { child in
                     Diagram(tree: child, node: node)
+                        .measureFrame(in: .named(coordinateSpace), id: child.id)
                 }
             }
+        }
+        .backgroundPreferenceValue(FrameKey.self) { frames in
+            let rootFrame = frames[tree.id]!
+            let childFrames: [(UUID, CGRect)] = frames.filter { $0.key != tree.id }
+            ForEach(childFrames, id: \.0) { _, childFrame in
+                Line(from: rootFrame.origin, to: childFrame.origin)
+                    .stroke(lineWidth: 1)
+            }
         }
         .coordinateSpace(name: coordinateSpace)
     }
@@ -61,8 +89,8 @@ struct AdvanceAlignmentView: View {
                 .padding()
                 .background(.tertiary)
         }
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
