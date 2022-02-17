028417788933f58db7454d47bd9edf163e5eba6a
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-17 12:31:30 +0000

    re-added workspace

--- Sources/Animation/Features/AnimatableProperties.swift
+++ Sources/Animation/Features/AnimatableProperties.swift
@@ -32,49 +32,8 @@ struct HueView: View {
     }
 }
 
-struct Animate_Alignment: View {
-    @State private var change = false
-    
-    var body: some View {
-        VStack(spacing: 20) {
-            VStack(alignment: change ? .trailing : .leading) {
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.blue)
-                    .frame(width: 100, height: 100)
-                    .animation(.default, value: change)
-                HStack {
-                    Spacer()
-                }
-            }
-            
-            HStack(alignment: change ? .bottom : .top) {
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.blue)
-                    .frame(width: 100, height: 100)
-                    .animation(.default, value: change)
-                VStack {
-                    Spacer()
-                }
-            }.frame(maxHeight: .infinity)
-            
-            Button("Change") {
-                change.toggle()
-            }
-            
-            GeometryReader { gp in
-                RoundedRectangle(cornerRadius:20)
-                .foregroundColor(.blue)
-                .frame(width: 100, height: 100)
-                .position(change ? CGPoint(x: 50, y: 50) : CGPoint(x: gp.size.width - 50, y: gp.size.height - 50))
-                .animation(.default, value: change)
-            }
-        }.font(.title)
-    }
-}
-
 struct HueViewPreview: PreviewProvider {
     static var previews: some View {
-        Animate_Alignment()
         HueView()
     }
 }
