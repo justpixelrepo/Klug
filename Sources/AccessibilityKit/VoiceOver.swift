f7240d7fc12ca3d49ddc3b854b22364ebeeb1348
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-10-24 11:14:16 +0000

    added `VoiceOver` code

---
+++ Sources/AccessibilityKit/VoiceOver.swift
@@ -0,0 +1,88 @@
+import UIKit
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text ("WWDC 2021")
+                .accessibilityAddTraits(.isHeader)
+            Text ("SwiftUI Accessibility")
+            Text ("Beyond the Basics")
+            Image(systemName:
+                    "checkmark.seal.fill")
+        }
+    }
+}
+
+class VoiceOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button = UIButton(frame: .zero)
+        button.setTitle("Hello World", for: .normal)
+
+     //   button.accessibilityTraits would be UIAccessibilityTraitButton
+     //   button.accessibilityLabel is set to "Hello World"
+        
+        multipleTraits()
+        
+    }
+    
+    private func accessibilityPostExample() {
+        // Simply post that the layout has changed
+        UIAccessibility.post(notification: .layoutChanged, argument: nil)
+
+        // Or, post it with more context on what's specifically changed
+        UIAccessibility.post(notification: .layoutChanged, argument: "Select birthday toggle now showing.")
+        
+        //add element to focus, use this with caution
+        //let birthdayPicker = UIPickerView()
+       // UIAccessibility.post(notification: .layoutChanged, argument: birthdayPicker)
+    }
+    
+    private func multipleTraits() {
+        let myTappableLabel = UILabel(frame: CGRect(x: 10, y: 100, width: 120, height: 44))
+        myTappableLabel.text = "Change Folder"
+        myTappableLabel.isUserInteractionEnabled = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(testFunc))
+        myTappableLabel.addGestureRecognizer(tapGesture)
+        
+        myTappableLabel.accessibilityTraits = [.button, .header]
+        
+        view.addSubview(myTappableLabel)
+    }
+    
+    @objc func testFunc() {
+        let alert = UIAlertController(title: "some info", message: "message", preferredStyle: .alert)
+        alert.addAction(.init(title: "Ok", style: .default))
+        present(alert, animated: true)
+        
+    }
+}
+
+
+#if DEBUG
+import SwiftUI
+struct VoiceOverViewControllerPreview: UIViewControllerRepresentable {
+    typealias UIViewControllerType = VoiceOverViewController
+    
+    func makeUIViewController(context: Context) -> VoiceOverViewController {
+        VoiceOverViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: VoiceOverViewController, context: Context) {
+    }
+    
+}
+
+@available(iOS 13.0, *)
+struct VoiceOverViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        VoiceOverViewControllerPreview().ignoresSafeArea()
+        ContentView()
+           
+    }
+}
+#endif
